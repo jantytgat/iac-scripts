ea05427233b2b5004d2003c1421380b0c1fae10d
Jan Tytgat <hello@jantytgat.com>
2022-12-30 19:15:39 +0000

    SSH Daemon configuration
    Signed-off-by: Jan Tytgat <hello@jantytgat.com>

---
+++ linux/os/generic/security/hardening/ssh/sshd_config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "########################################################################"
+echo "LINUX - OS - GENERIC - SECURITY - SSH config"
+echo "########################################################################"
+
+
+echo "### Remove old sshd_config"
+rm -f /etc/ssh/sshd_config
+
+echo "### Configuring new sshd_config"
+SCRIPT_PATH="$( cd $(dirname $0) && pwd)"
+cp $SCRIPT_PATH/templates/sshd_config.conf /etc/ssh/sshd_config
+chown root:root /etc/ssh/sshd_config
+chmod 644 /etc/ssh/sshd_config
+
+sed -i '/^session    optional     pam_motd.so/s//#&/' /etc/pam.d/sshd
+
+
+echo "########################################################################"
+echo " "
