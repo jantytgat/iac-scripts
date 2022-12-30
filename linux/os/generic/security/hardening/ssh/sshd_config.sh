9855092391ef1f1bf2bbace5c7673abb00c6784a
Jan Tytgat <hello@jantytgat.com>
2022-12-30 20:32:16 +0000

    SSH Daemon - updates
    Signed-off-by: Jan Tytgat <hello@jantytgat.com>

--- linux/os/generic/security/hardening/ssh/sshd_config.sh
+++ linux/os/generic/security/hardening/ssh/sshd_config.sh
@@ -9,7 +9,7 @@ rm -f /etc/ssh/sshd_config
 
 echo "### Configuring new sshd_config"
 SCRIPT_PATH="$( cd $(dirname $0) && pwd)"
-cp $SCRIPT_PATH/templates/sshd_config.conf /etc/ssh/sshd_config
+cp $SCRIPT_PATH/templates/* /etc/ssh/.
 chown root:root /etc/ssh/sshd_config
 chmod 644 /etc/ssh/sshd_config
 
