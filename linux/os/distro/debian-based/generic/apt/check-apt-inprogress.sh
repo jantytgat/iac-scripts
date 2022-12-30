6f62d101699305a6a78241d6fd02565bd9fc4046
Jan Tytgat <hello@jantytgat.com>
2022-12-30 16:57:17 +0000

    APT - Check if apt is running
    Signed-off-by: Jan Tytgat <hello@jantytgat.com>

---
+++ linux/os/distro/debian-based/generic/apt/check-apt-inprogress.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "########################################################################"
+echo "LINUX - OS - DISTRO - DEBIAN-BASED - GENERIC - Check if apt is running"
+echo "########################################################################"
+
+
+inProgress=true
+while [ $inProgress = true ]
+do
+    output=$(ps aux | grep apt-get | grep -i dpkg)
+    
+    if [ "$output" ]; then
+        echo "APT is running, sleep for 10 seconds"
+        sleep 10s
+
+        continue
+    fi
+    
+    inProgress=false
+done
+echo "Done."
+sleep 5s
+
+echo "########################################################################"
+echo " "
