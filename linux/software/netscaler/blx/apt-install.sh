7b536b4ce4d58ba8eaa937b94fefd07e87c0c1f3
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-05-09 23:02:31 +0200

    netscaler blx configuration script
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- linux/software/netscaler/blx/apt-install.sh
+++ linux/software/netscaler/blx/apt-install.sh
@@ -4,7 +4,6 @@ echo "### --> set variables"
 BLX_BASE_URL=$1
 BLX_VERSION=$2
 BLX_BUILD=$3
-HOST_DNS_NAMESERVER=$4
 
 echo "### --> add i386 architecture"
 dpkg --add-architecture i386
@@ -20,9 +19,3 @@ curl --output-dir /var/blx_install -O "${BLX_BASE_URL}/${BLX_VERSION}/${BLX_BUIL
 echo "### --> install netscaler blx"
 tar zxvf /var/blx_install*tar.gz
 DEBIAN_FRONTEND="noninteractive" apt-get install -q -y /var/blx_install/blx-deb-${BLX_VERSION}-${BLX_BUILD}/*.deb
-
-echo "### --> configure systemd post-blx.service"
-mkdir -p /root/.post-blx
-echo "HOST_DNS_NAMESERVER=$(/usr/bin/resolvectl status | grep "DNS Servers: " | sed -e 's/.*DNS Servers: //')" >> /root/.post-blx/environ
-cp /tmp/templates/post-blx.service /usr/lib/systemd/system/.
-systemctl enable post-blx.service
---
+++ linux/software/netscaler/blx/configure.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+echo "### configure netscaler blx"
+echo "### --> configure variables"
+
+BLX_WORKER_PROCESSES=$1
+BLX_IP=$2
+BLX_LICENSE_SERVER=$3
+BLX_LICENSE_BANDWIDTH=$4
+BLX_LICENSE_UNIT=$5
+BLX_LICENSE_EDITION=$6
+
+BLX_AUTO_ENABLE=$7
+BLX_AUTO_START=$8
+
+HOST_IP="$(ip addr | grep "inet.*eth0" | sed 's/^.*inet\s\([0-9.\/]*\)\s.*eth0$/\1/')"
+HOST_GW="$(ip route | grep default | sed 's/^default via\s\([0-9.]*\)\s.*$/\1/')"
+HOST_DNS_NAMESERVER="$(/usr/bin/resolvectl status | grep "DNS Servers: " | sed -e 's/.*DNS Servers: //')"
+
+echo "### --> configure systemd post-blx-start.service"
+mkdir -p /root/.post-blx-start
+echo "HOST_DNS_NAMESERVER=${HOST_DNS_NAMESERVER}" >> /root/.post-blx-start/environ
+cp /tmp/templates/post-blx.service /usr/lib/systemd/system/.
+systemctl enable post-blx-start.service
+
+cat > /etc/blx/blx.conf<<EOF
+blx-system-config
+{
+        worker-processes: $WORKER_PROCESSES
+        ipaddress: $BLX_IP
+        blx-managed-host: 1
+        host-ipaddress: $HOST_IP
+
+}
+
+static-routes
+{
+        default $HOST_GW
+}
+
+cli-cmds
+{
+        add ns licenseserver $BLX_LICENSE_SERVER
+        set ns capacity -bandwidth $BLX_LICENSE_BANDWIDTH -unit $BLX_LICENSE_UNIT -edition $BLX_LICENSE_EDITION
+}
+EOF
+
+echo "### --> enable and start netscaler blx"
+if $BLX_AUTO_ENABLE then
+    systemctl enable blx.service
+    if $BLX_AUTO_START then
+        systemctl start blx.service
+    fi
+fi
